883a649d156dffe8a467ea5a916d731606495c8b
Alexandru Roman <alexandruroman@Alexandrus-MacBook-Pro.local>
2020-05-25 17:28:14 +0100

    add initial project structure and json rpc service with hardcoded data

---
+++ alexroman.cv.api/alexroman.cv.api/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace alexroman.cv.api
+{
+    public class Startup
+    {
+        private readonly string _allowSpecificOrigins = nameof(_allowSpecificOrigins);
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: _allowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("https://alexroman.dev",
+                                            "https://www.alexroman.dev");
+                    });
+            });
+
+            // Fix for Anemonis.AspNetCore.JsonRpc using synchronous calls
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            services.AddJsonRpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseJsonRpc();
+
+            app.UseCors(_allowSpecificOrigins);
+        }
+    }
+}
